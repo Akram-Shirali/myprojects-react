efc8bc31eef53b48fb9fe8beead7873ebbd16a2a
Akram Shirali <a.shirali.eng@gmail.com>
2024-11-20 10:21:40 +0330

    add quiz-app project

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { question: "What's your name?", option1: "Akram", option2: "Ali", option3: "Amin", option4: "Hana", ans: 1 },
+  { question: "which one is True?", option1: "1 + 1 = 3", option2: "6 - 2 = 3", option3: "5 + 5 = 10", option4: "1 + 0 = 2", ans: 3 },
+  { question: "What's your age?", option1: "30", option2: "27", option3: "29", option4: "34", ans: 4 },
+  { question: "what's your job?", option1: "Teacher", option2: "Engineer", option3: "Architect", option4: "Shopkeeper", ans: 2 },
+  { question: "Where do you live?", option1: "Iran", option2: "America", option3: "India", option4: "Netherlands", ans: 1 },
+];
